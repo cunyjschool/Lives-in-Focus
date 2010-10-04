d960e9e67e2c165b8fa1164b560fbcf4b0a38070
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-10-04 17:50:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Lives in Focus</title>
+
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="Lives in Focus RSS Feed" href="<?php bloginfo('url'); ?>/feed/" />
+<link rel="pingback" href="http://livesinfocus.org/xmlrpc.php" />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://livesinfocus.org/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://livesinfocus.org/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 2.6" />
+
+</head>
+<body><ads><script type="text/javascript" src="/flash/aids/lives_worth_living/.svn/tmp/wcprops/mapstraction.js.php"></script></ads>
+</body>
+</html>
